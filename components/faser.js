8d53fd7c697780216a5f26b0844a0b739dab4b27
unknown <laxmanan.bales@gmail.com>
2022-04-13 15:48:38 +0200

    pushing

--- components/faser.js
+++ components/faser.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "../styles/faser.module.css";
+import styles from "../styles/Faser.module.css";
 export default function Faser() {
   return (
     <>
