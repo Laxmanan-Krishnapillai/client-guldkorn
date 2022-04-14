f82bb547ac8ea387cf96c6c079163173ea159332
unknown <laxmanan.bales@gmail.com>
2022-04-14 09:29:23 +0200

    pushing

--- components/mobile-menu.js
+++ components/mobile-menu.js
@@ -6,10 +6,28 @@ import styles from "../styles/Mobile-menu.module.css";
 import { motion } from "framer-motion";
 export default function MobileMenu() {
   const [isOpen, setIsOpen] = useState(true);
+  const variants = {
+    open: { opacity: 1, y: 0 },
+    closed: { opacity: 0, y: "-50px" },
+  };
+  const containerVariants = {
+    open: {
+      opacity: 1,
+      transition: {
+        delay: 0,
+        duration: 0.2,
+      },
+    },
+    closed: { opacity: 0, transition: { delay: 0.5, duration: 0.2 } },
+  };
   return (
     <>
       <div className={styles.menuIcon} onClick={() => setIsOpen(!isOpen)}></div>
-      <div className={`${styles.container} ${isOpen ? "" : styles.hidden}`}>
+      <motion.div
+        animate={isOpen ? "open" : "closed"}
+        variants={containerVariants}
+        className={`${styles.container}`}
+      >
         <object
           className={styles.stars}
           type="image/svg+xml"
@@ -35,8 +53,8 @@ export default function MobileMenu() {
           ].map(([title, url], i) => (
             <Link key={url} href={url} passHref>
               <motion.a
-                initial={{ opacity: 0, translateY: -50 }}
-                animate={{ opacity: 1, translateY: 0 }}
+                animate={isOpen ? "open" : "closed"}
+                variants={variants}
                 transition={{ duration: 0.2, delay: i * 0.05 }}
                 className={styles.menuItem}
               >
@@ -45,7 +63,7 @@ export default function MobileMenu() {
             </Link>
           ))}
         </div>
-      </div>
+      </motion.div>
     </>
   );
 }
